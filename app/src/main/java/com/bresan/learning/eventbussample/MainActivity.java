2eda221778ba7e8b4dfec35be3f5684cb2d45600
Rodrigo Bresan <rcbresan@gmail.com>
2016-06-15 22:14:49 -0300

    Subscriber for event bus and handler for running the IntentService

--- app/src/main/java/com/bresan/learning/eventbussample/MainActivity.java
+++ app/src/main/java/com/bresan/learning/eventbussample/MainActivity.java
@@ -1,9 +1,15 @@
 package com.bresan.learning.eventbussample;
 
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, CustomIntentService.class);
+                startService(intent);
+            }
+        }, 5000);
     }
 
     @Override
@@ -25,4 +40,8 @@ public class MainActivity extends AppCompatActivity {
         EventBus.getDefault().unregister(this);
     }
 
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void myMethodForSubscriber(IntentServiceResult intentServiceResult) {
+        Toast.makeText(this, intentServiceResult.getmResultValue(), Toast.LENGTH_SHORT).show();
+    }
 }
