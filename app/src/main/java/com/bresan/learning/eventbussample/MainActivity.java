575231f5b8dbc3ecbbfe87da5e106e02ed10565b
Rodrigo Bresan <rcbresan@gmail.com>
2016-06-15 21:48:55 -0300

    Register and unregister

--- app/src/main/java/com/bresan/learning/eventbussample/MainActivity.java
+++ app/src/main/java/com/bresan/learning/eventbussample/MainActivity.java
@@ -3,6 +3,8 @@ package com.bresan.learning.eventbussample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.greenrobot.eventbus.EventBus;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        EventBus.getDefault().unregister(this);
+    }
+
 }
